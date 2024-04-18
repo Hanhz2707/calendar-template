cf713d9a32bf164fc8d1ad41e2e66d630ab31eac
Pham Huy Anh <144563745+Hanhz2707@users.noreply.github.com>
2024-04-18 10:50:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -33,21 +33,7 @@ const timeFormate = document.querySelector('.time-formate');
 const dateFormate = document.querySelector('.date-formate');
 
 
-// month_picker.onclick = () => {
-//     month_list.classList.remove('hideonce'); 
-//     month_list.classList.remove('hide');
-//     month_list.classList.add('show');
 
-//     dayTextFormate.classList.remove('showtime');
-//     dayTextFormate.classList.add('hidetime');
-
-//     timeFormate.classList.remove('showtime');
-//     timeFormate.classList.add('hideTime');
-
-//     dateFormate.classList.remove('showtime');
-//     dateFormate.classList.add('hideTime');
-
-// };
 month_picker.onclick = () => {
     month_list.classList.remove('hideonce');
     month_list.classList.remove('hide');
@@ -86,17 +72,7 @@ const generateCalendar = (month, year) => {
     calendar_header_year.innerHTML = year;
 
 
-//     for(let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++){
-//         let day = document.createElement('div');
-//         if(i >= first_day.getDay()){
-//             day.innerHTML = i - first_day.getDay() + 1;
-//             if(i - first_day.getDay() + 1 === currentDate.getDate() && year === currentDate.getFullYear() && month === currentDate.getMonth()){
-//                 day.classList.add('current-date');
-//             };
-//         };
-//         calendar_days.appendChild(day);
-//     };
-// };
+
 for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
 
     let day = document.createElement('div');
@@ -116,26 +92,6 @@ for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
 };
 
 
-// let month_list = calendar.querySelector('.month-list');
-// month_names.forEach(
-//     (e, index) => {
-//     let month = document.createElement('div');
-//     month.innerHTML = `<div>${e}</div>`;
-//     month_list.append(month);
-//     month.onclick = () => {
-//         currentMonth.value = index;
-//         generateCalendar(currentMonth.value, currentYear.value);
-//         month_list.classList.replace('show', 'hide');
-//         dayTextFormate.classList.remove('hideTime');
-//         dayTextFormate.classList.add('showtime');
-
-//         timeFormate.classList.remove('hideTime');
-//         timeFormate.classList.add('showtime');
-
-//         dateFormate.classList.remove('hideTime');
-//         dateFormate.classList.add('showtime');
-//     };
-// });
 let month_list = calendar.querySelector('.month-list');
 month_names.forEach((e, index) => {
   let month = document.createElement('div');
